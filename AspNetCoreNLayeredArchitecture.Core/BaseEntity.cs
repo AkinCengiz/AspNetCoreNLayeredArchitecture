ad360dfeb96a68d84396505acd58c372864c53ca
akincengiz <akincengiz@outlook.com.tr>
2024-06-04 17:41:25 +0300

    Add project files.

---
+++ AspNetCoreNLayeredArchitecture.Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreNLayeredArchitecture.Core;
+public abstract class BaseEntity
+{
+	public int Id { get; set; }
+	public DateTime CreateAt { get; set; }
+	public DateTime UpdateAt { get; set; }
+	public bool IsDeleted { get; set; }
+	public bool IsActive { get; set; }
+}
