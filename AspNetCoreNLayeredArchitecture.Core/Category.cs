85255a00ecb7fa0c965bcea98408695db1d27bd1
akincengiz <akincengiz@outlook.com.tr>
2024-06-04 17:44:15 +0300

    Product and category entities created.

--- AspNetCoreNLayeredArchitecture.Core/Category.cs
+++ AspNetCoreNLayeredArchitecture.Core/Category.cs
@@ -7,4 +7,6 @@ using System.Threading.Tasks;
 namespace AspNetCoreNLayeredArchitecture.Core;
 public class Category : BaseEntity
 {
+	public string Name { get; set; }
+	public ICollection<Product> Products { get; set; }	
 }
--- AspNetCoreNLayeredArchitecture.Core/Product.cs
+++ AspNetCoreNLayeredArchitecture.Core/Product.cs
@@ -7,4 +7,9 @@ using System.Threading.Tasks;
 namespace AspNetCoreNLayeredArchitecture.Core;
 public class Product : BaseEntity
 {
+	public string Name { get; set; }
+	public int Stock { get; set; }
+	public decimal Price { get; set; }
+	public int CategoryId { get; set; }
+	public Category Category { get; set; }
 }
