e63964dfdea2e06623d65bf8544edc57711fd4fa
akincengiz <akincengiz@outlook.com.tr>
2024-06-05 16:45:42 +0300

    AutoMapper library added and configuration completed.

---
+++ AspNetCoreNLayeredArchitecture.Service/Mapping/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreNLayeredArchitecture.Core.DTOs;
+using AspNetCoreNLayeredArchitecture.Core.Models;
+using AutoMapper;
+
+namespace AspNetCoreNLayeredArchitecture.Service.Mapping;
+public class MapProfile : Profile
+{
+	public MapProfile()
+	{
+		CreateMap<Product, ProductDto>().ReverseMap();
+		CreateMap<Category, CategoryDto>().ReverseMap();
+		CreateMap<ProductFeature, ProductFeatureDto>();
+		CreateMap<ProductUpdateDto, Product>();
+	}
+}
--- AspNetCoreNLayeredArchitecture.WebAPI/Program.cs
+++ AspNetCoreNLayeredArchitecture.WebAPI/Program.cs
@@ -6,6 +6,8 @@ using AspNetCoreNLayeredArchitecture.Core.UnitOfWorks;
 using AspNetCoreNLayeredArchitecture.Repository;
 using AspNetCoreNLayeredArchitecture.Repository.Repositories;
 using AspNetCoreNLayeredArchitecture.Repository.UnitOfWorks;
+using AspNetCoreNLayeredArchitecture.Service.Mapping;
+using AspNetCoreNLayeredArchitecture.Service.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreNLayeredArchitecture.WebAPI;
@@ -22,7 +24,8 @@ public class Program
 		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 		builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 		builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-		//builder.Services.AddScoped(typeof(IService<>), typeof(GenericRepository<>));
+		builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+		builder.Services.AddAutoMapper(typeof(MapProfile));
 		builder.Services.AddDbContext<AppDbContext>(options =>
 		{
 			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), options =>
