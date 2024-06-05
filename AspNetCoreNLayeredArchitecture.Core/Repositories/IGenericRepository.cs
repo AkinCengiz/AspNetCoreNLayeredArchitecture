6626be3aadc57db34b08c21d09aae2b8659ae142
akincengiz <akincengiz@outlook.com.tr>
2024-06-05 10:13:15 +0300

    IRespository and IService interfaces created.

---
+++ AspNetCoreNLayeredArchitecture.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreNLayeredArchitecture.Core.Repositories;
+public interface IGenericRepository<T> where T : class
+{
+	IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null);
+	Task<T> GetByIdAsync(int id);
+	IQueryable<T> Where(Expression<Func<T, bool>> filter);
+	Task<bool> AnyAsync(Expression<Func<T, bool>> filter);
+	Task AddAsync(T entity);
+	Task AddRangeAsync(IEnumerable<T> entities);
+	void Update(T entity);
+	void Delete(T entity);
+	void DeleteRange(IEnumerable<T> entities);
+	
+}
